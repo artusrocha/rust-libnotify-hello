c1e762cbcbf19a263e42cb394a2eb949b2f3d347
Artus Rocha <artusrocha@gmail.com>
2018-06-26 21:30:01 -0300

    Rocket http server Notify (static hello message yet) on http request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
 extern crate notify_rust;
 use notify_rust::Notification;
-fn main() {
-	Notification::new()
-    .summary("Hello, world!")
-		.body("Teste de notificação")
-		.icon("dialog-information")
-		.show().unwrap();
+
+#[get("/")]
+fn index() -> &'static str {
+  Notification::new()
+     .summary("Hello, world!")                                                                                                                
+     .body("Teste de notificação")
+     .icon("dialog-information")
+     .show().unwrap();
+
+  "Hello, world!"
 }
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
